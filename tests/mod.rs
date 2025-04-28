66ac9bd3312eab16e85e770b78098003a6179d27
Christian Schwarz <cmrs@cmrs.io>
2025-04-28 12:05:52 +0200

    only enable ui tests on nightly
    this avoids errors caused by differing error messages

--- tests/mod.rs
+++ tests/mod.rs
@@ -2,6 +2,9 @@
 
 mod macros;
 
+// only enable ui tests on nightly to avoid failures due to
+// differing error messages
+#[rustversion::attr(not(nightly), ignore)]
 #[test]
 fn ui() {
     let t = trybuild::TestCases::new();
