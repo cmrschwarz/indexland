4c6ff6366d43fd02651b6454e14536b313adc853
Christian Schwarz <cmrs@cmrs.io>
2025-07-26 16:04:34 +0200

    run ui tests on msrv instead of nightly

--- tests/integration.rs
+++ tests/integration.rs
@@ -2,17 +2,21 @@
 
 mod macros;
 
-// only enable ui tests on nightly to avoid failures due to
-// differing error messages
-#[rustversion::attr(not(nightly), ignore)]
+#[rustversion::attr(not(stable(1.87)), ignore = "ui tests only run on 1.87 (MSRV)")]
 #[test]
 fn ui() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/ui/*.rs");
 }
 
-#[cfg(feature = "derive")]
-#[rustversion::attr(not(nightly), ignore)]
+#[rustversion::attr(not(stable(1.87)), ignore = "ui tests only run on 1.87 (MSRV)")]
+#[rustversion::attr(
+    stable(1.87),
+    cfg_attr(
+        not(all(feature = "derive", feature = "std")),
+        ignore = "ui_derive tests require the \"derive\" and \"std\" feature flags"
+    )
+)]
 #[test]
 fn ui_derive() {
     let t = trybuild::TestCases::new();
